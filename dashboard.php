44dfe2c02d9ecce5084e3bf80ff97c4daa719dcb
Candace Hendricks <candiiihendricks@gmail.com>
2023-12-09 18:24:03 -0500

    Edited dashboard display, added js and css accordingly

--- dashboard.php
+++ dashboard.php
@@ -4,13 +4,15 @@ include_once 'layout.php';
 <!DOCTYPE html>
 <html lang="en">
 <?= page_header("Dashboard") ?>
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="dboard.css">
 <!-- Content for Page Here -->
 <div class="col py-3 pt-4 page-content">
     <!-- insert here -->
+    <h2 class="text-start fw-bold mb-4">Dashboard</h2>
     <div class="card">
         <div class="card-body">
-            <h2 class="text-start fw-bold mb-4">Dashboard</h2>
+            
             <hr />
             <!-- filter -->
 
@@ -22,7 +24,7 @@ include_once 'layout.php';
 
     </div>
 </div>
-
+<script src="dboard.js"></script>
 <?= page_footer() ?>
 
 </html>
