21da1e7f9765b7dc0a656a4407d4bc67fcf6e4a4
Candace Hendricks <candiiihendricks@gmail.com>
2023-12-10 12:10:37 -0500

    Change of page name

--- contact-details.php
+++ contact-details.php
@@ -4,7 +4,7 @@ include_once 'layout.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<?=page_header()?>
+<?=page_header(View Contact Details)?>
 
 <!-- Content for Page Here -->
 <div class="col py-3 pt-4 page-content">
